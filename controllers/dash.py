4d9775d6c88963016a598c5cf9e6beba84933dda
Jorge Alberto Landaverde <jalbertolandav@gmail.com>
2013-12-15 05:31:30 -0800

    refactored to use Jinja templates

--- controllers/dash.py
+++ controllers/dash.py
@@ -2,7 +2,7 @@ from controllers import base
 class DashHandler(base.BaseHandler):
 	def get(self): 
 		#self.response.write("Dash")
-		self.render("dash.html",title="SSS Admin")
+		self.render("dash.html",title="Dashboard")
 	def post(self):
 		self.response.write("Dash")
 
