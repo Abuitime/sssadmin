86343e02618b2482fbc3da929ba3519edd5239e7
Jorge Alberto Landaverde <jalbertolandav@gmail.com>
2013-12-15 07:27:15 -0800

    table populated with jinja

--- controllers/events.py
+++ controllers/events.py
@@ -6,26 +6,9 @@ from models import event
 class EventsHandler(base.BaseHandler):
     def get(self):
         user = users.get_current_user()
-        events = event.Event.all()
-        e_list = ""
+        e = event.Event.all()
 
-        #list events
-        for e in events:
-            ebutton = """
-                <div class="btn-group"><a href="/admin/initevent?elaunch=%s" 
-                class="btn btn-default">Launch</a></div>
-            """ %e.eid
-
-            e_list += """
-            <tr>
-                <td>%s</td>
-                <td>%s</td>
-                <td>%s</td>
-                <td>%s</td>
-                <td>%s</td>
-            </tr>
-            """ %(e.name, e.location, e.description, e.date, ebutton)
-        self.render("events.html", events = e_list, logged_user = user.nickname())
+        self.render("events.html", events = e, logged_user = user.nickname())
 
     def post(self):
         #create and add event to datastore
